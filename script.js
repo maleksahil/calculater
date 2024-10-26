3a0fca34b349f75210b37cb58787d88e6bbc654f
sahil <maleksahil9315@gmail.com>
2024-10-26 08:09:01 +0530

    calculator

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let string = "";
 
 let arr = Array.from(buttons);
 
-arr.forEach(button => {
+arr.forEach((button) => {
 
     button.addEventListener('click',(e) =>{
 
@@ -21,9 +21,11 @@ arr.forEach(button => {
              string = "";
              input.value = string;
 
-        }else if(e.target.innerHTML == 'DEL'){
-
-            string = string.substring(0, string.length-1);
+        }else if (e.target.innerHTML == 'DEL') {
+                string = string.toString();
+                string = string.substring(0, string.length - 1);
+                input.value = string;
+            
         }else{
 
         string += e.target.innerHTML;
